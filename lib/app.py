3ab7a0aa0bba0cdce9834e92c8cfb81bf436e0be
codeviith <codeviith@Cinya.>
2023-09-11 14:33:45 -0400

    Running Python Code Lab

---
+++ lib/app.py
@@ -0,0 +1,7 @@
+# print("Hello World!", end="\n") # The \n adds a new line at the end of the code
+#                                 # so that the next print function will start at the next line.
+# print("Hello sun!", end="!!\n")
+# print("Hello sky!", end="!!!\n")
+
+print("Hello World! Pass this test, please.")
+
